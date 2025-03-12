0db02338b4eb3cd1fb05694d845d268b2de6e963
haochen <sumtsui@gmail.com>
2025-03-12 15:43:11 +0800

    remove db volume

--- start-db.sh
+++ start-db.sh
@@ -7,7 +7,6 @@ docker run --name postgres-playground-db \
   -e POSTGRES_PASSWORD=$DB_PASSWORD \
   -e POSTGRES_USER=$DB_USER \
   -e POSTGRES_DB=$DB_NAME \
-  -v postgres-pg-playground-data:/var/lib/postgresql/data \
   -p $DB_PORT:5432 \
   --rm \
   -d postgres
